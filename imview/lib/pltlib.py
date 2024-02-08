9a814d38fc369e8b5efcb206a3de80d035c152ed
atdunwoody <146408382+atdunwoody@users.noreply.github.com>
2024-02-08 13:34:59 -0700

    deleted shebang line

--- imview/lib/pltlib.py
+++ imview/lib/pltlib.py
@@ -234,7 +234,7 @@ def add_cbar(ax, mappable, label=None, arr=None, clim=None, cbar_kwargs=cbar_kwa
     #Set colorbar to be opaque, even if image is transparent
     cbar.set_alpha(1)
     #Need this to update after alpha change
-    cbar.draw_all()
+    cbar._draw_all()
     return cbar
 
 """
